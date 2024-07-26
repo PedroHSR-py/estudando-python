95be261dbe8f0d0d2087c98101341d99548e54e4
PedroHSR-py <phenriquers10@gmail.com>
2024-07-26 19:14:48 -0300

    pega a distancia em metros e mostra a mesma distancia em diferentes medidas

---
+++ ex007.py
@@ -0,0 +1,14 @@
+m = float(input('Uma distancia em metros: '))
+print('A medida de {} corresponde a'.format(m))
+km = m /1000
+hm = m /100
+dam = m /10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+print('{}Km'.format(km))
+print('{}Hm'.format(hm))
+print('{}Dam'.format(dam))
+print('{}Dm'.format(dm))
+print('{}Cm'.format(cm))
+print('{}Mm'.format(mm))
