d383e1d69a90c5ad3604f2dabe2111c4b7e79ad9
PedroHSR-py <phenriquers10@gmail.com>
2024-07-26 19:09:27 -0300

    calcula o dobro, triplo e raiz quadrada do nÃúmero

---
+++ ex005.py
@@ -0,0 +1,9 @@
+n = int(input('digite um numero: '))
+n1 = n * 2
+n2 = n * 3
+n3 = n ** (1/2)
+
+print('O dobro de {} vale {}'.format(n, n1))
+print('O triplo de {} vale {}'.format(n, n2))
+print('A raiz quadrada de {} é {}'.format(n, n3))
+
